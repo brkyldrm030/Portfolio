e21ebe08ba6fc4d5fa52a3ecd00fa6d66a5d05ac
brkyldrm030 <burak.yildirim030@outlook.de>
2025-09-14 00:33:20 +0200

    Update Portfolio Seite

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,76 +14,80 @@ const Projects = () => {
 
       <div className="grid grid-cols-1 lg:grid-2 xl:grid-cols-3 gap-5 ml-5">
         <div
-          className="text-left space-y-2 border-2 hover:scale-105 transition-all duration-200
-        cursor-pointer px-8 py-10 border-[#1A273A] rounded-lg hover:bg-[#C24D2C]/30"
+          className="text-center space-y-6 border-2 hover:scale-105 transition-all duration-200
+             cursor-pointer px-8 py-10 border-[#1A273A] rounded-lg hover:bg-[#C24D2C]/30"
         >
           <h1 className="text-3xl font-semibold">Astronaut</h1>
-          <h3 className="text-xl text-gray-700">Tech Stack</h3>
-          <p className="text-sm text-gray-600">
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-          </p>
+          <a
+            href="https://brkyldrm030.github.io/Astronaut/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="mt-4 px-6 py-2 bg-[#C24D2C] text-white rounded-lg hover:bg-[#a33e23] transition">
+              Klick mich!
+            </button>
+          </a>
         </div>
+
         <div
-          className="text-left space-y-2 border-2 hover:scale-105 transition-all duration-200
-        cursor-pointer px-8 py-10 border-[#1A273A] rounded-lg hover:bg-[#C24D2C]/30"
+          className="text-center space-y-6 border-2 hover:scale-105 transition-all duration-200
+             cursor-pointer px-8 py-10 border-[#1A273A] rounded-lg hover:bg-[#C24D2C]/30"
         >
           <h1 className="text-3xl font-semibold">Designer</h1>
-          <h3 className="text-xl text-gray-700">Tech Stack</h3>
-          <p className="text-sm text-gray-600">
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-          </p>
+          <a
+            href="https://brkyldrm030.github.io/Aysulix-Design/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="mt-4 px-6 py-2 bg-[#C24D2C] text-white rounded-lg hover:bg-[#a33e23] transition">
+              Klick mich!
+            </button>
+          </a>
+        </div>
+
+        <div
+          className="text-center space-y-6 border-2 hover:scale-105 transition-all duration-200
+             cursor-pointer px-8 py-10 border-[#1A273A] rounded-lg hover:bg-[#C24D2C]/30"
+        >
+          <h1 className="text-3xl font-semibold">Coming soon</h1>
+          <a href="" target="_blank" rel="noreferrer">
+            <button className="mt-4 px-6 py-2 bg-[#C24D2C] text-white rounded-lg hover:bg-[#a33e23] transition">
+              Klick mich!
+            </button>
+          </a>
         </div>
         <div
-          className="text-left space-y-2 border-2 hover:scale-105 transition-all duration-200
-        cursor-pointer px-8 py-10 border-[#1A273A] rounded-lg hover:bg-[#C24D2C]/30"
+          className="text-center space-y-6 border-2 hover:scale-105 transition-all duration-200
+             cursor-pointer px-8 py-10 border-[#1A273A] rounded-lg hover:bg-[#C24D2C]/30"
         >
-          <h1 className="text-3xl font-semibold">Project</h1>
-          <h3 className="text-xl text-gray-700">Tech Stack</h3>
-          <p className="text-sm text-gray-600">
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-          </p>
+          <h1 className="text-3xl font-semibold">Coming soon</h1>
+          <a href="" target="_blank" rel="noreferrer">
+            <button className="mt-4 px-6 py-2 bg-[#C24D2C] text-white rounded-lg hover:bg-[#a33e23] transition">
+              Klick mich!
+            </button>
+          </a>
         </div>
         <div
-          className="text-left space-y-2 border-2 hover:scale-105 transition-all duration-200
-        cursor-pointer px-8 py-10 border-[#1A273A] rounded-lg hover:bg-[#C24D2C]/30"
+          className="text-center space-y-6 border-2 hover:scale-105 transition-all duration-200
+             cursor-pointer px-8 py-10 border-[#1A273A] rounded-lg hover:bg-[#C24D2C]/30"
         >
-          <h1 className="text-3xl font-semibold">Project</h1>
-          <h3 className="text-xl text-gray-700">Tech Stack</h3>
-          <p className="text-sm text-gray-600">
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-          </p>
+          <h1 className="text-3xl font-semibold">Coming soon</h1>
+          <a href="" target="_blank" rel="noreferrer">
+            <button className="mt-4 px-6 py-2 bg-[#C24D2C] text-white rounded-lg hover:bg-[#a33e23] transition">
+              Klick mich!
+            </button>
+          </a>
         </div>
         <div
-          className="text-left space-y-2 border-2 hover:scale-105 transition-all duration-200
-        cursor-pointer px-8 py-10 border-[#1A273A] rounded-lg hover:bg-[#C24D2C]/30"
+          className="text-center space-y-6 border-2 hover:scale-105 transition-all duration-200
+             cursor-pointer px-8 py-10 border-[#1A273A] rounded-lg hover:bg-[#C24D2C]/30"
         >
-          <h1 className="text-3xl font-semibold">Project</h1>
-          <h3 className="text-xl text-gray-700">Tech Stack</h3>
-          <p className="text-sm text-gray-600">
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-          </p>
-        </div>
-        <div
-          className="text-left space-y-2 border-2 hover:scale-105 transition-all duration-200
-        cursor-pointer px-8 py-10 border-[#1A273A] rounded-lg hover:bg-[#C24D2C]/30"
-        >
-          <h1 className="text-3xl font-semibold">Project</h1>
-          <h3 className="text-xl text-gray-700">Tech Stack</h3>
-          <p className="text-sm text-gray-600">
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-          </p>
+          <h1 className="text-3xl font-semibold">Coming soon</h1>
+          <a href="" target="_blank" rel="noreferrer">
+            <button className="mt-4 px-6 py-2 bg-[#C24D2C] text-white rounded-lg hover:bg-[#a33e23] transition">
+              Klick mich!
+            </button>
+          </a>
         </div>
       </div>
     </div>
